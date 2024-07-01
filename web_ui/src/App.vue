637079816ee0557398d4dd112341ac2237d5b84f
voleshko <vyacheslavoleshko@gmail.com>
2024-07-01 14:37:14 -0700

    Added Chart js UI

--- web_ui/src/App.vue
+++ web_ui/src/App.vue
@@ -1,38 +1,37 @@
 <template>
-  <div id="app" class="container my-3">
-    <h3>Github Repositories</h3>
-
-    <div class="card mt-3">
-      <div class="card-header">Repositories</div>
-      <div class="card-body">
-        <div class="input-group input-group-sm">
-          <button @click="getStats(repo)" class="btn btn-sm btn-primary" v-for="(repo, i) in repositories" :key="i">
-            <p>{{repo}}</p>
-          </button>
-        </div><br/>
+  <div class="nav-side-menu">
+      <div class="brand">Github Repositories</div>
+      <i class="fa fa-bars fa-2x toggle-btn" data-toggle="collapse" data-target="#menu-content"></i>
+      <div class="menu-list">
+              <ul id="menu-content" class="menu-content collapse out">
+              <template v-for="(repo, i) in repositories" :key="i">
+                  <li @click="getStats(repo)">
+                    <i class="fa fa-github fa-lg"></i> {{repo}}
+                  </li>
+              </template>
+              </ul>
       </div>
-    </div>
-    <div v-if="stats">
-        <p> Total commits: {{stats.totalCommits}} </p>
-        <p> Total committers: {{stats.totalCommitters}} </p>
-        <p> <b>Top-5:</b> </p>
-        <div v-for="contributor in stats.topFiveContributors" v-bind:key="contributor.email">
-            <p>{{contributor.email}}: {{contributor.commitCount}}</p>
-
-        </div>
-    </div>
   </div>
+    <div v-if="stats" style='margin-left:400px'>
+        <p> Total commits: <b>{{stats.totalCommits}} </b></p>
+        <p>Total committers: <b>{{stats.totalCommitters}} </b></p>
+            <BarChart :chartData="chartData"/>
+    </div>
+
 </template>
 
 <script>
+import BarChart from './components/BarChart.vue';
 const baseURL = "http://localhost:8070";
 
 export default {
   name: "App",
+  components: { BarChart },
   data() {
     return {
       repositories: [],
       stats: null,
+      chartData: null
     }
   },
 
@@ -96,6 +95,22 @@ export default {
         };
 
         this.stats = result.data;
+
+        // construct data for chart
+        let labels = this.stats.topFiveContributors.map(contributor => contributor.email);
+        let commitCount = this.stats.topFiveContributors.map(contributor => contributor.commitCount);
+        this.chartData = {
+                    labels: labels,
+                    datasets: [
+                      {
+                        label: 'Top-5 by commits count',
+                        data: commitCount,
+                        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                        borderColor: 'rgba(75, 192, 192, 1)',
+                        borderWidth: 1
+                      }
+                    ]
+                  };
       } catch (err) {
         this.stats = err.message;
       }
@@ -106,7 +121,303 @@ export default {
 
 <style>
 #app {
-  max-width: 600px;
   margin: auto;
+}
+
+.nav-side-menu {
+  overflow: auto;
+  font-family: verdana;
+  font-size: 12px;
+  font-weight: 200;
+  background-color: #2e353d;
+  position: fixed;
+  top: 0px;
+  width: 300px;
+  height: 100%;
+  color: #e1ffff;
+}
+.nav-side-menu .brand {
+  background-color: #23282e;
+  line-height: 50px;
+  display: block;
+  text-align: center;
+  font-size: 14px;
+}
+.nav-side-menu .toggle-btn {
+  display: none;
+}
+.nav-side-menu ul,
+.nav-side-menu li {
+  list-style: none;
+  padding: 0px;
+  margin: 0px;
+  line-height: 35px;
+  cursor: pointer;
+  /*
+    .collapsed{
+       .arrow:before{
+                 font-family: FontAwesome;
+                 content: "\f053";
+                 display: inline-block;
+                 padding-left:10px;
+                 padding-right: 10px;
+                 vertical-align: middle;
+                 float:right;
+            }
+     }
+*/
+}
+.nav-side-menu ul :not(collapsed) .arrow:before,
+.nav-side-menu li :not(collapsed) .arrow:before {
+  font-family: FontAwesome;
+  content: "\f078";
+  display: inline-block;
+  padding-left: 10px;
+  padding-right: 10px;
+  vertical-align: middle;
+  float: right;
+}
+.nav-side-menu ul .active,
+.nav-side-menu li .active {
+  border-left: 3px solid #d19b3d;
+  background-color: #4f5b69;
+}
+.nav-side-menu ul .sub-menu li.active,
+.nav-side-menu li .sub-menu li.active {
+  color: #d19b3d;
+}
+
+.nav-side-menu ul .sub-menu li.active a,
+.nav-side-menu li .sub-menu li.active a {
+  color: #d19b3d;
+}
+.nav-side-menu ul .sub-menu li,
+.nav-side-menu li .sub-menu li {
+  background-color: #181c20;
+  border: none;
+  line-height: 28px;
+  border-bottom: 1px solid #23282e;
+  margin-left: 0px;
+}
+.nav-side-menu ul .sub-menu li:hover,
+.nav-side-menu li .sub-menu li:hover {
+  background-color: #020203;
+}
+.nav-side-menu ul .sub-menu li:before,
+.nav-side-menu li .sub-menu li:before {
+  font-family: Fontawesome;
+  content: "\f105";
+  display: inline-block;
+  padding-left: 20px;
+  padding-right: 10px;
+  vertical-align: middle;
+}
+.nav-side-menu li {
+  padding-left: 0px;
+  border-left: 3px solid #2e353d;
+  border-bottom: 1px solid #23282e;
+}
+.nav-side-menu li a {
+  text-decoration: none;
+  color: #e1ffff;
+}
+.nav-side-menu li a i {
+  padding-left: 10px;
+  width: 20px;
+  padding-right: 20px;
+}
+.nav-side-menu li:hover {
+  border-left: 3px solid #d19b3d;
+  background-color: #4f5b69;
+  -webkit-transition: all 1s ease;
+  -moz-transition: all 1s ease;
+  -o-transition: all 1s ease;
+  -ms-transition: all 1s ease;
+  transition: all 1s ease;
+}
+@media (max-width: 767px) {
+  .nav-side-menu {
+    position: relative;
+    width: 100%;
+    margin-bottom: 10px;
+  }
+  .nav-side-menu .toggle-btn {
+    display: block;
+    cursor: pointer;
+    position: absolute;
+    right: 10px;
+    top: 10px;
+    z-index: 10 !important;
+    padding: 3px;
+    background-color: #ffffff;
+    color: #000;
+    width: 40px;
+    text-align: center;
+  }
+  .brand {
+    text-align: left !important;
+    font-size: 22px;
+    padding-left: 20px;
+    line-height: 50px !important;
+  }
+}
+@media (min-width: 767px) {
+  .nav-side-menu .menu-list .menu-content {
+    display: block;
+  }
+}
+body {
+  margin: 0px;
+  padding: 0px;
+}
+
+.nav-side-menu ul .sub-menu ul .sub-line li.active,
+.nav-side-menu li .sub-menu li .sub-line li.active {
+  color: #d19b3d;
+}
+
+.nav-side-menu ul .sub-menu li .sub-line li.active a,
+.nav-side-menu li .sub-menu li .sub-line li.active a {
+  color: #d19b3d;
+}
+.nav-side-menu ul .sub-menu li .sub-line li,
+.nav-side-menu li .sub-menu li .sub-line li {
+  background-color: #181c20;
+  border: none;
+  line-height: 28px;
+  border-bottom: 1px solid #23282e;
+  margin-left: 0px;
+}
+.nav-side-menu ul .sub-menu li .sub-line li:hover,
+.nav-side-menu li .sub-menu li .sub-line li:hover {
+  background-color: #020203;
+}
+.nav-side-menu ul .sub-menu li .sub-line li:before,
+.nav-side-menu li .sub-menu li .sub-line li:before {
+  font-family: FontAwesome;
+  content: "\f105";
+  display: inline-block;
+  padding-left: 100px;
+  padding-right: 10px;
+  vertical-align: middle;
+}
+
+.nav-side-menu .sub-menu li {
+  padding-left: 20px;
+  border-left: 3px solid #2e353d;
+  border-bottom: 1px solid #23282e;
+}
+.nav-side-menu .sub-menu li a {
+  text-decoration: none;
+  color: #e1ffff;
+}
+.sub-menu li a i {
+  padding-left: 10px;
+  width: 20px;
+  padding-right: 20px;
+}
+.nav-side-menu li .sub-menu li:hover {
+  border-left: 3px solid #d19b3d;
+  background-color: #4f5b69;
+  -webkit-transition: all 1s ease;
+  -moz-transition: all 1s ease;
+  -o-transition: all 1s ease;
+  -ms-transition: all 1s ease;
+  transition: all 1s ease;
+}
+@media (max-width: 767px) {
+ .nav-side-menu .sub-menu {
+    position: relative;
+    width: 100%;
+    margin-bottom: 10px;
+  }
+}
+.nav-side-menu  .sub-menu .toggle-btn {
+    display: block;
+    cursor: pointer;
+    position: absolute;
+    right: 10px;
+    top: 10px;
+    z-index: 10 !important;
+    padding: 3px;
+    background-color: #ffffff;
+    color: #000;
+    width: 40px;
+    text-align: center;
+  }
+
+  .sub-line ul .sub-press li.active,
+.sub-line li .sub-press li.active {
+  color: #d19b3d;
+}
+
+.sub-line ul .sub-press li.active a,
+.sub-line li .sub-press li.active a {
+  color: #d19b3d;
+}
+.sub-line ul .sub-press li,
+.sub-line li .sub-press li {
+  background-color: #181c20;
+  border: none;
+  line-height: 28px;
+  border-bottom: 1px solid #23282e;
+  margin-left: 0px;
+}
+.sub-line ul .sub-press li:hover,
+.sub-line li .sub-press li:hover {
+  background-color: #020203;
+}
+.sub-line ul .sub-press li:before,
+.sub-line li .sub-press li:before {
+  font-family: Arial;
+  content: "\f105";
+  display: inline-block;
+  padding-left: 50px;
+  padding-right: 10px;
+  vertical-align: middle;
+}
+
+.sub-line li {
+  padding-left: 20px;
+  border-left: 3px solid #2e353d;
+  border-bottom: 1px solid #23282e;
+}
+.sub-line li a {
+  text-decoration: none;
+  color: #e1ffff;
+}
+.sub-line li a i {
+  padding-left: 50px;
+  width: 20px;
+  padding-right: 20px;
+}
+.sub-line li:hover {
+  border-left: 3px solid #d19b3d;
+  background-color: #4f5b69;
+  -webkit-transition: all 1s ease;
+  -moz-transition: all 1s ease;
+  -o-transition: all 1s ease;
+  -ms-transition: all 1s ease;
+  transition: all 1s ease;
+}
+@media (max-width: 767px) {
+  .sub-line {
+    position: relative;
+    width: 100%;
+    margin-bottom: 10px;
+  }
+  .sub-line .toggle-btn {
+    display: block;
+    cursor: pointer;
+    position: absolute;
+    right: 10px;
+    top: 10px;
+    z-index: 10 !important;
+    padding: 3px;
+    background-color: #ffffff;
+    color: #000;
+    width: 40px;
+    text-align: center;
+  }
 }
 </style>
---
+++ web_ui/src/components/BarChart.vue
@@ -0,0 +1,39 @@
+<template>
+  <Bar
+    id="top-five-bar-chart"
+    :options="chartOptions"
+    :data="chartData"
+  />
+</template>
+
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  props: {
+      chartData: {
+        type: Object,
+        required: true
+      }
+    },
+    data() {
+      return {
+        options: {
+          responsive: true,
+          maintainAspectRatio: false,
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      };
+    }
+  };
+
+</script>
